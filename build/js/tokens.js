19eb31e5a3e39eda9082fe415819229d0fcdf7a4
rhenandias <diasrhenan@gmail.com>
2023-12-15 21:59:32 -0300

    First commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 16 Dec 2023 00:36:13 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
